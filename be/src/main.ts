c1558b475e297ff599541a93505caf0537e45b53
Delmar <andrej.delmar@siemens.com>
2020-11-15 14:19:15 +0100

    local docker config

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,15 +1,15 @@
-import * as Koa from 'koa';
-import * as logger from 'koa-logger';
-import { connectDB } from './database';
-import * as models from './app/models';
-import * as dotenv from 'dotenv';
+import * as Koa from "koa";
+import * as logger from "koa-logger";
+import { connectDB } from "./database";
+import * as models from "./app/models";
+import * as dotenv from "dotenv";
 dotenv.config();
-import { router } from './app/router';
-import { ApolloServer, PubSub } from 'apollo-server-koa';
-import schemas from './app/schemas'
-import resolvers from './app/resolvers'
+import { router } from "./app/router";
+import { ApolloServer, PubSub } from "apollo-server-koa";
+import schemas from "./app/schemas";
+import resolvers from "./app/resolvers";
 // import spoonApi from './app/services/spoontacular.service';
-import services from './app/services';
+import services from "./app/services";
 
 export const pubsub = new PubSub();
 
@@ -17,28 +17,35 @@ const PORT = process.env.PORT || 3000;
 
 const app = new Koa();
 const server = new ApolloServer({
-    typeDefs: schemas,
-    resolvers,
-    context: ({ctx}) => {
-         return {
-            user: services.authService.authenticate(ctx),
-            ...services,
-            ctx,
-            models,
-        }
-    }
+  typeDefs: schemas,
+  resolvers,
+  context: ({ ctx }) => {
+    return {
+      user: services.authService.authenticate(ctx),
+      ...services,
+      ctx,
+      models,
+    };
+  },
 });
 
-server.applyMiddleware({ app, cors: {
+server.applyMiddleware({
+  app,
+  cors: {
     origin: process.env.CORS,
     credentials: true,
-} });
+  },
+});
 
-connectDB().then(() => {
+connectDB()
+  .then(() => {
     app.keys = [process.env.COOKIE_SECRET];
-    app.use(logger())
-    app.use(router.routes()).use(router.allowedMethods())
+    app.use(logger());
+    app.use(router.routes()).use(router.allowedMethods());
 
-    const appServer = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+    const appServer = app.listen(PORT, () =>
+      console.log(`Listening on port ${PORT}`)
+    );
     server.installSubscriptionHandlers(appServer);
-})
+  })
+  .catch((err) => console.log("Problem connecting to the database: ", err));
--- fe/src/websocketSetup.ts
+++ fe/src/websocketSetup.ts
@@ -1,33 +1,36 @@
-import { WebSocketLink } from 'apollo-link-ws';
-import { split } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { getMainDefinition } from 'apollo-utilities';
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { getMainDefinition } from "apollo-utilities";
 
-const ssl = process.env.SSL_ENABLED ? 's' : '';
-const uri = process.env.API_URL_DOMAIN ? `http${ssl}://${process.env.API_URL_DOMAIN}/graphql` : 'http://localhost:3000/graphql';
-const wsUri = process.env.API_URL_DOMAIN ? `ws${ssl}://${process.env.API_URL_DOMAIN}/graphql` : 'ws://localhost:3000/graphql';
-
+const ssl = process.env.SSL_ENABLED ? "s" : "";
+const uri = process.env.API_URL_DOMAIN
+  ? `http${ssl}://${process.env.REACT_APP_API_URL_DOMAIN}/graphql`
+  : "http://localhost:3000/graphql";
+const wsUri = process.env.API_URL_DOMAIN
+  ? `ws${ssl}://${process.env.REACT_APP_API_URL_DOMAIN}/graphql`
+  : "ws://localhost:3000/graphql";
 
 const wsLink = new WebSocketLink({
   uri: wsUri,
   options: {
-    reconnect: true
-  }
+    reconnect: true,
+  },
 });
 
 const httpLink = new HttpLink({
   uri,
-  credentials: 'include',
+  credentials: "include",
 });
 
 export const link = split(
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-          definition.kind === 'OperationDefinition' &&
-          definition.operation === 'subscription'
-        );
-    },
-    wsLink,
-    httpLink,
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
 );
